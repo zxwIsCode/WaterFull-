0fa88530313a88250d916347a5ab36f3b871e35f
张小伟 <1824496534@qq.com>
2016-11-07 14:44:52 +0800

    第一次文件初始化

---
+++ WaterFull/WaterFull/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WaterFull
+//
+//  Created by 李保东 on 16/11/7.
+//  Copyright © 2016年 李保东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
